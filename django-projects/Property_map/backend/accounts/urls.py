3dce3dd64c5ecb03fdd44c8c36c3fc56cd206c20
syed-wassay <syedwasay6300@gmail.com>
2023-10-02 10:40:52 +0500

    also added urls.py

---
+++ django-projects/Property_map/backend/accounts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import RegisterView, LogoutAPIView, SetNewPasswordAPIView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI, RequestPasswordResetEmail
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('signup/', RegisterView.as_view(), name="signup"),
+    path('signin/', LoginAPIView.as_view(), name="signin"),
+    path('logout/', LogoutAPIView.as_view(), name="logout"),
+    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('request-reset-email/', RequestPasswordResetEmail.as_view(),
+         name="request-reset-email"),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete', SetNewPasswordAPIView.as_view(),
+         name='password-reset-complete')
+]
